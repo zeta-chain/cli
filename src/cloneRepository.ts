138a9766dfd14a9b94030bfb9308a8082cab3e48
Denis Fadeev <denis@fadeev.org>
2025-01-26 18:06:32 +0300

    feat: clone again if repo older than one day

--- src/cloneRepository.ts
+++ src/cloneRepository.ts
@@ -6,9 +6,21 @@ export const cloneRepository = async (
   tempDir: string,
   branchName: string,
   options: any,
-  isVerbose: boolean,
+  isVerbose: boolean
 ) => {
-  if (!options.cache || !fs.existsSync(tempDir)) {
+  const ONE_DAY_IN_MS = 24 * 60 * 60 * 1000;
+
+  const shouldClone = async () => {
+    if (!fs.existsSync(tempDir)) return true;
+
+    const stats = await fs.stat(tempDir);
+    const lastModified = new Date(stats.mtime).getTime();
+    const now = Date.now();
+
+    return now - lastModified > ONE_DAY_IN_MS;
+  };
+
+  if (!options.cache || (await shouldClone())) {
     if (fs.existsSync(tempDir)) {
       if (isVerbose) console.log("Removing cached repository...");
       await fs.remove(tempDir);
