d8a33b03ee488260ae5b1727d103bff78efb083b
Denis Fadeev <denis@fadeev.org>
2025-01-26 17:56:06 +0300

    codeowners

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import { cloneRepository } from "./cloneRepository";
 import { copyExample } from "./copyExample";
 import { getExampleDirectories } from "./getExampleDirectories";
 import { promptForExample } from "./promptForExample";
+import clear from "clear";
 
 const program = new Command();
 const REPO_URL = "https://github.com/zeta-chain/example-contracts.git";
@@ -18,14 +19,14 @@ program
   .name("create-universal-contracts")
   .description(
     "CLI tool for creating universal contracts on ZetaChain.\n" +
-      "For more information, visit: https://zetachain.com/docs",
+      "For more information, visit: https://zetachain.com/docs"
   )
   .option("--no-cache", "Bypass cached repository and re-clone")
   .option("--verbose", "Enable verbose logging")
   .option(
     "--output <directory>",
     "Specify custom output directory or name",
-    process.cwd(),
+    process.cwd()
   );
 
 program.parse(process.argv);
@@ -36,6 +37,7 @@ const outputDir = options.output;
 
 const main = async () => {
   try {
+    clear();
     await cloneRepository(REPO_URL, TEMP_DIR, BRANCH_NAME, options, isVerbose);
     const directories = await getExampleDirectories(EXAMPLES_DIR);
     const chosenExample = await promptForExample(directories);
