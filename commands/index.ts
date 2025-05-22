26f0588ec9c4cb2391058f4843c2a2340a8d8f6c
Denis Fadeev <denis@fadeev.org>
2025-05-22 11:55:31 +0300

    refactor

--- commands/index.ts
+++ commands/index.ts
@@ -1,11 +1,9 @@
 #!/usr/bin/env node
 import { Command } from "commander";
 import deploy from "./deploy.js";
-import run from "../src/commands/run.js";
 
 const program = new Command();
 
 deploy(program);
-run(program);
 
 program.parse(process.argv);
