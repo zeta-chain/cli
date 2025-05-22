b662d97e76882b50f8356e870f74789a3f35c5bb
Denis Fadeev <denis@fadeev.org>
2025-05-22 11:47:33 +0300

    added run command

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { accountsCommand, solanaCommand } from "@zetachain/toolkit/commands";
 import { Command } from "commander";
 
 import { newCommand } from "./commands/new";
+import registerRun from "./commands/run";
 
 const program: Command = new Command();
 
@@ -16,5 +17,6 @@ program.addCommand(newCommand);
 program.addCommand(localnetCommand);
 program.addCommand(accountsCommand);
 program.addCommand(solanaCommand);
+registerRun(program);
 
 program.parse(process.argv);
