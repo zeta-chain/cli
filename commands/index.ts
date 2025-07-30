256c4e52718677945d24757150eb267e5c474eb4
Hernan Clich <hernan.g.clich@gmail.com>
2025-07-30 16:08:07 -0300

    Fix import

--- commands/index.ts
+++ commands/index.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import { Command } from "commander";
 
-import { deployCommand } from "./deploy.js";
+import { deployCommand } from "./deploy";
 
 const program = new Command().helpCommand(false).addCommand(deployCommand);
 
